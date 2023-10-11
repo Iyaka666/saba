4c9f120d887baedd7a19bba679112ac25b9e03b4
Iyaka666 <duendecum@gmail.com>
2023-10-10 20:54:08 -0500

    contiene informacion que se puede usar para multiples estilos

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+    colors:{
+        textPrimary:'#222222',
+        textSecondary:'#5c5454',
+        primary:'#ea6565',
+    },
+    fontSizes:{
+        body:14,
+        subheading:12
+    },
+    fonts:{
+        main:'System'
+    },
+    fontWeight:{
+        normal:'400',
+        bold:'700'
+    }
+}
+
+export default theme
