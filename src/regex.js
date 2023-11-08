8a3707e3d47010045895010a298ee8a804e9ecc7
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2023-11-08 08:38:04 -0500

    Se modifico isOnlyText

--- src/regex.js
+++ src/regex.js
@@ -1,3 +1,3 @@
 export const isNumberAndOthers = new RegExp("^[0-9-]*$")
 export const isPasswordValid = new RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$")
-export const isOnlyText = new RegExp("^[a-zA-Z]*$")
+export const isOnlyText = new RegExp("^[a-zA-ZÁ-Úá-úüÜñÑ\s]+$")
