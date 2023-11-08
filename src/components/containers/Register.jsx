4d07eca744996560def7e36844cd1cd6afd1fed8
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2023-11-07 22:38:51 -0500

    agregando el esquema

--- src/components/containers/Register.jsx
+++ src/components/containers/Register.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import {View} from 'react-native'
+import {View, Text, Pressable} from 'react-native'
 import TextField from './generals/Textfield.jsx'
 import PasswordField from './generals/Passwordfield.jsx'
 
 const Register = () => {
     return(
+    <View>
         <View>
-            <View>
-                <Header></Header>
-            </View>
-            <View>
-                <TextField/>
-                <TextField/>
-                <TextField/>
-                <TextField/>
-                <PasswordField/>
-                <PasswordField/>
-            </View>
-            <View>
-                <Footer></Footer>
-            </View>
+            <Header></Header>
         </View>
+        <View>
+            <TextField placeholder='Nombres' autoCapitalize='words'/>
+            <TextField placeholder='Apellidos' autoCapitalize='words'/>
+            <TextField placeholder='Código de estudiante'/>
+            <TextField placeholder='correo electrónico'/>
+            <PasswordField placeholder='contraseña' initSecure/>
+            <PasswordField placeholder='confirmar contraseña' initSecure/>
+            <Pressable>
+                <Text>Registrar</Text>
+            </Pressable>
+            <Text>¿Ya tienes una cuenta? <Text>inicia sesion</Text></Text>
+        </View>
+        <View>
+            <Footer></Footer>
+        </View>
+    </View>
     )
 }
 
