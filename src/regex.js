0062fc0dbfc101faa167a2f613babd74299780a2
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2023-11-14 13:15:45 -0500

    Cambio de una expresion regular
    se modifico la variable que contiene una expresion regular llamada isNumberAndOthers

--- src/regex.js
+++ src/regex.js
@@ -1,4 +1,4 @@
-export const isNumberAndOthers = new RegExp("^[0-9-]*$")
+export const isNumberAndOthers = new RegExp("^[0-9-]+$")
 export const isPasswordValid = new RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$")
 export const isOnlyText = new RegExp("^[a-zA-ZÁ-Úá-úüÜñÑ\s]+$")
 export const isValidEmail = new RegExp("^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")
