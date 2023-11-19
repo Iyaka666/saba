3a9f8b0c0ba1a127e9d6f664da306829ba8a3b9c
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2023-11-19 16:04:58 -0500

    Acerca de archivo functions
    Se ha renombrado el archivo functions para las funciones que se encargan de la validacion de valores validos además de mejorar de esa manera la reutilización de código

---
+++ src/validationValidValues.js
@@ -0,0 +1,24 @@
+import {isNumberAndOthers, isPasswordValid} from './regex.js'
+import {Alert} from 'react-native'
+
+export const verifyInputUser = (inputUser, setInputUser, ) => {
+    setInputUser(inputUser)
+    if(!isNumberAndOthers.test(inputUser)) {
+        Alert.alert(
+            title='Usuario invalido', 
+            message='El usuario solo acepta números y "-" ejemplo: 12345678-9'
+            )
+        setInputUser('')
+    }
+}
+
+export const verifyInputPassword = (inputPassword, setInputPassword) => {
+    setInputPassword(inputPassword)
+    if(!isPasswordValid.test(inputPassword)) {
+        Alert.alert(
+            title='Contraseña Invalida',
+            message='Debe contener mínimo 8 letras, tener un número y tener un caracter especial ej:( !,",#,$,%,&,/,(,) )'
+            )
+        setInputPassword('')
+    }
+}
