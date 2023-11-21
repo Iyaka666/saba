cdf9218cede1d81175d5c28f76f60bfb9b05f727
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2023-11-20 23:10:32 -0500

    Se agregaron más funciones

--- src/validationValidValues.js
+++ src/validationValidValues.js
@@ -1,5 +1,55 @@
-import {isNumberAndOthers, isPasswordValid} from './regex.js'
+import {
+    isNumberAndOthers, 
+    isPasswordValid, 
+    isOnlyText, 
+    isValidCode, 
+    isValidEmail
+} from './regex.js'
 import {Alert} from 'react-native'
+
+export const verifyInputName = (inputName, setInputName) => {
+    setInputName(inputName)
+    if(!isOnlyText.test(inputName)){
+        Alert.alert(
+            title='Nombres Invalido',
+            message='El nombre solo puede contener letras y acentos'
+            )
+        setInputName('')
+    }
+}
+
+export const verifyInputLastname = (inputLastname, setInputLastname) => {
+    setInputLastname(inputLastname)
+    if(!isOnlyText.test(inputLastname)){
+        Alert.alert(
+            title='Apellidos Invalidos',
+            message='El apellido solo puede contener letras y acentos'
+            )
+        setInputLastname('')
+    }
+}
+
+export const verifyInputCode = (inputCode, setInputCode) => {
+    setInputCode(inputCode)
+    if(!isValidCode.test(inputCode)){
+        Alert.alert(
+            title='Código Invalido',
+            message='Debe contener mínimo 8 letras, tener un número y tener un caracter especial ej:( !,",#,$,%,&,/,(,) )'
+            )
+        setInputCode('')
+    }
+}
+
+export const verifyInputEmail = (inputEmail, setInputEmail) => {
+    setInputEmail(inputEmail)
+    if(!isValidEmail.test(inputEmail)){
+        Alert.alert(
+            title='Correo electrónico invalido',
+            message='Verifique que solo tenga simbolos como "-","." y "@" '
+            )
+        setInputEmail('')
+    }
+}
 
 export const verifyInputUser = (inputUser, setInputUser ) => {
     setInputUser(inputUser)
@@ -21,4 +71,20 @@ export const verifyInputPassword = (inputPassword, setInputPassword) => {
             )
         setInputPassword('')
     }
+}
+
+export const validEqualPassword = (currentPassword, newPassword) => {
+    if(currentPassword !== newPassword){
+        Alert.alert(title='No hay Coincidencia',
+        message='Las contraseñas no coinciden')
+    }
+}
+export const validFieldEmpty = (...content) => {
+    if( content.every(str => str.trim() === '') ) {
+        Alert.alert(
+        title='Campo de texto vacío',
+        message='No debe quedar ningún campo de texto vacío')
+        return false
+    }
+    return true
 }
