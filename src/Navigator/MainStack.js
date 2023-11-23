a5397ba6860a119b21df0d25745d677d08adad39
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2023-11-23 07:42:52 -0500

    Se creo el MainStack que contiene todos las screen nombradas

--- src/Navigator/MainStack.js
+++ src/Navigator/MainStack.js
@@ -1,12 +1,48 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import Login from "../screen/Login"
+import { createStackNavigator } from "@react-navigation/stack"
+import {NavigationContainer} from "@react-navigation/native"
+import Login from "../screen/Login.jsx"
+import Register from "../screen/Register.jsx"
+import ForgotPassword from "../screen/ForgotPassword.jsx"
+import Verification from "../screen/Verification.jsx"
+import ResetPassword from "../screen/ResetPassword.jsx"
+import Home from "../screen/Home.jsx"
 
-const Stack = createNativeStackNavigator()
+
+const Stack = createStackNavigator()
 
 const MainStack = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="Login" component={Login}/>
-        </Stack.Navigator>
+        <NavigationContainer>
+            <Stack.Navigator
+            screenOptions={{
+                headerShown:false
+            }}>
+                <Stack.Screen 
+                name="Login" 
+                component={Login}/>
+                
+                <Stack.Screen 
+                name="Register" 
+                component={Register}/>
+                
+                <Stack.Screen 
+                name="ForgotPassword" 
+                component={ForgotPassword}/>
+                
+                <Stack.Screen 
+                name="Verification" 
+                component={Verification}/>
+                
+                <Stack.Screen 
+                name="ResetPassword" 
+                component={ResetPassword}/>
+                
+                <Stack.Screen 
+                name="Home" 
+                component={Home}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
+
+export default MainStack
