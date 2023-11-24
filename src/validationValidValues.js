4709807b40b540ebd876f680b2dc19bfcae6bd25
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2023-11-24 15:21:41 -0500

    Se agregarón comentarios al archivo

--- src/validationValidValues.js
+++ src/validationValidValues.js
@@ -7,83 +7,70 @@ import {
 import {Alert} from 'react-native'
 
 //Valida que el Nombre solo contenga letras o acentos
-export const verifyInputName = (name, setName) => {
+export const verifyName = (name, setName) => {
     if(!isOnlyText.test(name)){
-        Alert.alert(
-            title='Nombres Invalido',
-            message='El nombre sólo puede contener letras y acentos'
-            )
+        Alert.alert('Nombres Invalido','El nombre sólo puede contener letras y acentos')
         setName('')
         return false
     }
     return true
 }
 
-//Valida que el apellido contenga letras o acentos
-export const verifyInputLastname = (lastname, setLastname) => {
+//Valida que el apellido sólo se pueda contener letras y letras con acentos
+export const verifyLastname = (lastname, setLastname) => {
     if(!isOnlyText.test(lastname)){
-        Alert.alert(
-            title='Apellidos Invalidos',
-            message='El apellido solo puede contener letras y acentos'
-            )
+        Alert.alert('Apellidos Invalidos','El apellido solo puede contener letras y acentos')
         setLastname('')
         return false
     }
     return true
 }
 
-//Valida que el código de logeo funcione correctamente
-export const verifyInputCode = (code, setCode) => {
+//Valida que el código de logeo sea ingresado correctamente
+export const verifyCode = (code, setCode) => {
     if(!isValidCode.test(code)){
-        Alert.alert(
-            title='Código Invalido',
-            message='Debe contener mínimo 8 letras, tener un número y tener un caracter especial ej:( !,",#,$,%,&,/,(,) )'
-            )
+        Alert.alert('Código Invalido',
+        'Debe ser algo como 2012345-1234 ó una cédula de 7 a 10 digitos más su letra correspondiente')
         setCode('')
         return false
     }
     return true
 }
 
-//valida que la direccion de correo electronico este bien escrita
-export const verifyInputEmail = (email, setEmail) => {
+//valida que la direccion de correo electrónico sea ingresada correctamente
+export const verifyEmail = (email, setEmail) => {
     if(!isValidEmail.test(email)){
-        Alert.alert(
-            title='Correo electrónico invalido',
-            message='Verifique que solo tenga simbolos como "-","." y "@" '
-            )
+        Alert.alert('Correo electrónico invalido',
+        'Verifique que solo tenga simbolos como "-","." y "@" ')
         setEmail('')
         return false
     }
     return true
 }
 
-export const verifyInputPassword = (password, setPassword) => {
+//Valida que la contraseña sea ingresada correctamente
+export const verifyPassword = (password, setPassword) => {
     if(!isPasswordValid.test(password)) {
-        Alert.alert(
-            title='Contraseña Invalida',
-            message='Debe contener mínimo 8 letras, tener un número y tener un caracter especial ej:( !,",#,$,%,&,/,(,) )'
-            )
+        Alert.alert('Contraseña Invalida','Debe contener mínimo 8 cáracteres')
         setPassword('')
         return false
     }
     return true
 }
 
+//Valida que dos contraseñas iguales
 export const validEqualPassword = (currentPassword, newPassword) => {
     if(currentPassword !== newPassword){
-        Alert.alert(title='No hay Coincidencia',
-        message='Las contraseñas no coinciden')
+        Alert.alert('No hay Coincidencia','Las contraseñas no coinciden')
         return false
     }
     return true
 }
 
+//Valida que las cadenas de caracteres de n datos sean espacios vacios
 export const validFieldEmpty = (...content) => {
     if( content.every(str => str.trim() === '') ) {
-        Alert.alert(
-        title='Campo de texto vacío',
-        message='No debe quedar ningún campo de texto vacío')
+        Alert.alert('Campo de texto vacío','No debe quedar ningún campo de texto vacío')
         return false
     }
     return true
